24e9772ffac6085eb3dc3a9486b730ff98739c71
borisgrunwald <borisgrunwald@gmail.com>
2019-11-24 21:30:09 +0100

    stuff

---
+++ Migrations/20191124160258_User.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class User : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MyPropert",
+                table: "Users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MyPropert",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
